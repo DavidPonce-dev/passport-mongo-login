d9a707a8b4d1184761ca411d8e4e0db0d2293253
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2022-02-23 03:52:58 -0300

    creando ruta de perfil

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ router.get('/signup', (req, res, next)=>{
 })
 
 router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/',
+    successRedirect: '/profile',
     failureRedirect: '/signup',
     passReqToCallback: true
 }))
@@ -22,6 +22,11 @@ router.get('/signin', (req, res, next)=>{
 })
 
 router.post('/signin', (req, res, next)=>{
+    
+})
+
+router.get('/profile', (req, res, next)=>{
+    res.render('profile')
     
 })
 
