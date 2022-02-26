a31d111e5c0f2f05c4d6a14e84511c940484d27d
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2022-02-26 14:00:27 -0300

    resolviendo problema de contexto this en arrow function

--- src/models/user.js
+++ src/models/user.js
@@ -7,11 +7,11 @@ const userSchema = new Schema({
     password: String
 })
 
-userSchema.methods.encryptPassword = (password) => {
+userSchema.methods.encryptPassword = function (password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(10))
 }
 
-userSchema.methods.comparePassword = (password) => {
+userSchema.methods.comparePassword = function (password) {
     return bcrypt.compareSync(password, this.password)
 }
 
