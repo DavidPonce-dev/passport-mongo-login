d6990680b72509632618d21b3412e9015807c549
DavidPonce-dev <60764076+DavidPonce-dev@users.noreply.github.com>
2022-02-26 13:59:55 -0300

    creando logica y ruta del login

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -5,16 +5,28 @@ const User = require('../models/user')
 
 const register = async (req, email, password, done) => {
     
-    const user =  User.findOne({email})
-    if (user) return done(null, false, req.flash('signupMessage', 'el correo ya existe'))
-    
-    const newUser = new User()
-    newUser.email = email
-    newUser.password = user.encryptPassword(password)
-    await user.save()
-    done(null, user)
+    const user = await User.findOne({email})
+    if (user) done(null, false, req.flash('signupMessage', 'el correo ya existe'))
+    else {
+        const newUser = new User()
+        newUser.email = email
+        newUser.password = newUser.encryptPassword(password)
+        await newUser.save()
+        done(null, newUser)
+    }
 }
 
+const login = async (req, email, password, done) => {
+    const user = await User.findOne({email: email});
+    if(!user) {
+      return done(null, false, req.flash('signinMessage', 'No User Found'));
+    }
+    if(!user.comparePassword(password)) {
+      return done(null, false, req.flash('signinMessage', 'Incorrect Password'));
+    }
+    return done(null, user);
+  }
+  
 passport.serializeUser((user, done)=> {
     done(null, user.id)
 })
@@ -23,6 +35,12 @@ passport.deserializeUser(async (id, done)=> {
     const user = await User.findById(id)
     done(null, user)
 })
+
+passport.use('local-signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, login));
 
 passport.use('local-signup', new LocalStrategy({
     usernameField: 'email',
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,12 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 
-router.get('/', (req, res, next)=>{
+router.get('/', (req, res)=>{
     res.render('index')
 })
 
-router.get('/signup', (req, res, next)=>{
+router.get('/signup', (req, res)=>{
     res.render('signup')
-    
 })
 
 router.post('/signup', passport.authenticate('local-signup', {
@@ -17,17 +16,18 @@ router.post('/signup', passport.authenticate('local-signup', {
     passReqToCallback: true
 }))
 
-router.get('/signin', (req, res, next)=>{
-    
+router.get('/signin', (req, res)=>{
+    res.render('signin')    
 })
 
-router.post('/signin', (req, res, next)=>{
-    
-})
+router.post('/signin',  passport.authenticate('local-signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+    passReqToCallback: true
+}))
 
-router.get('/profile', (req, res, next)=>{
+router.get('/profile', (req, res)=>{
     res.render('profile')
-    
 })
 
 module.exports = router
